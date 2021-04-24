c2f9df5609836d2fa1b9e120a4ed5ed3c7ec63be
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2021-04-25 03:16:09 +0800

    add modal, RWD, some logic

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-// input Minute
-let inputMin = 1;
+// // input Minute
+// let inputMin = 1;
 
-// to sec
-let inputSec = inputMin * 60;
-console.log(inputSec, " sec");
+// // to sec
+// let inputSec = inputMin * 60;
+// console.log(inputSec, " sec");
 
-let counter = function (sec) {
-  let hours;
-  let mins;
-  let secs;
-  let countDown = setInterval(() => {
-    hours = ~~(sec / 3600);
-    console.log(hours);
-    mins = ~~((sec % 3600) / 60);
-    console.log(mins);
-    secs = ~~(sec % 60);
-    console.log(secs);
-    sec -= 1;
-    // console.log(countDown);
-    if (sec < 0) {
-      clearInterval(countDown);
-    }
-  }, 1000);
-};
+// let counter = function (sec) {
+//   let hours;
+//   let mins;
+//   let secs;
+//   let countDown = setInterval(() => {
+//     hours = ~~(sec / 3600);
+//     console.log(hours);
+//     mins = ~~((sec % 3600) / 60);
+//     console.log(mins);
+//     secs = ~~(sec % 60);
+//     console.log(secs);
+//     sec -= 1;
+//     // console.log(countDown);
+//     if (sec < 0) {
+//       clearInterval(countDown);
+//     }
+//   }, 1000);
+// };
 
-counter(inputSec);
+// counter(inputSec);
+
+let a = 1;
+console.log(a.toString().padStart(2, "0"));
--- script/main.js
+++ script/main.js
@@ -6,6 +6,7 @@ let vvue = new Vue({
     countDownSec: "00",
     countDownMin: "00",
     countDownHour: "00",
+    countDown: "",
   },
   methods: {
     inputing(e) {
@@ -20,40 +21,61 @@ let vvue = new Vue({
       if (this.workFor === 0 || this.workFor === "") {
         alert("沒有努力專心的寶貝是不能成為番茄寶貝的!");
       } else {
-        let inputSec = this.workFor * 60;
+        $("#cancelBtn").attr("disabled", false);
 
-        // let countDownHour = this.countDownHour;
-        // let countDownMin = this.countDownMin;
-        // let countDownSec = this.countDownSec;
+        let inputSec = this.workFor * 60;
 
         let counter = (sec) => {
           let hours;
           let mins;
           let secs;
-          let countDown = setInterval(() => {
+          this.countDown = setInterval(() => {
             // this is window object!!!!
             // console.log("this is ", this);
             // clearInterval(countDown);
-            console.log("it is ", this);
 
             hours = ~~(sec / 3600);
-            this.countDownHour = hours;
-            // console.log(countDownHour);
+            // .toString().padStart(2, "0");
+
+            this.countDownHour = hours.toString().padStart(2, "0");
             mins = ~~((sec % 3600) / 60);
-            this.countDownMin = mins;
-            // console.log(countDownMin);
+            this.countDownMin = mins.toString().padStart(2, "0");
+
             secs = ~~(sec % 60);
-            this.countDownSec = secs;
-            // console.log(countDownSec);
+            this.countDownSec = secs.toString().padStart(2, "0");
+
+            // not working???
+            // this.countDownSec = ~~(sec % 60).toString().padStart(2, "0");
+            // this.countDownSec = secs;
+
             sec -= 1;
             if (sec < 0) {
-              clearInterval(countDown);
+              this.workFor = "";
+              this.restFor = 0;
+              this.countDownSec = "00";
+              this.countDownMin = "00";
+              this.countDownHour = "00";
+              alert(
+                "恭喜寶貝成為變成番茄寶貝了~~~\n休息個" +
+                  this.restFor +
+                  "分鐘吧~"
+              );
+              clearInterval(this.countDown);
             }
           }, 1000);
         };
 
         counter(inputSec);
       }
+    },
+    cancelTiming() {
+      $("#cancelBtn").attr("disabled", true);
+      clearInterval(this.countDown);
+      this.workFor = "";
+      this.restFor = 0;
+      this.countDownSec = "00";
+      this.countDownMin = "00";
+      this.countDownHour = "00";
     },
   },
 });
